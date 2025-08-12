35e044c5c6ac905618ce8dfcb8be6d9578b6851c
Asif Jirayat <asifjirayat@gmail.com>
2025-08-12 18:34:12 +0530

    Changed logic for displaying clock. Added a blinking colon

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -13,19 +13,34 @@ const Clock = () => {
     };
   }, []);
 
+  // Format date
   const formatDate = (date) => {
     const options = { weekday: "long", month: "long", day: "numeric" };
     return new Intl.DateTimeFormat("en-US", options).format(date);
   };
 
-  const formatTime = (date) => {
-    const options = { hour: "numeric", minute: "numeric" };
-    return new Intl.DateTimeFormat("en-US", options).format(date);
-  };
+  // Blinking colon
+  const showColon = time.getSeconds() % 2 === 0;
+
+  // Format time
+  let hours = time.getHours();
+  const ampm = hours > 12 ? "PM" : "AM";
+  hours = hours % 12;
+  hours = hours ? hours : 12; // Hour 0 should be 12
+  const minutes = time.getMinutes().toString().padStart(2, "0");
+  const seconds = time.getSeconds().toString().padStart(2, "0");
 
   return (
     <div className="flex flex-col items-center">
-      <div className="text-8xl font-light">{formatTime(time)}</div>
+      <div className="text-8xl font-light">
+        <span>
+          {hours}:{minutes}
+        </span>
+        <span style={{ visibility: showColon ? "visible" : "hidden" }}>:</span>
+        <span>
+          {seconds} {ampm}
+        </span>
+      </div>
       <div className="text-3xl font-light mt-2">{formatDate(time)}</div>
     </div>
   );
